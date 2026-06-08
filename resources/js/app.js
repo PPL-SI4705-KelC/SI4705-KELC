d57cee38602a530539cd54eb514e80f2dcf55537
SleepyyJago <syahdanma3009@gmail.com>
2026-06-09 00:26:51 +0700

    test

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-//
-=======
 import './bootstrap';
 
 import Alpine from 'alpinejs';
@@ -8,4 +5,3 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
->>>>>>> 14f647be00457c2be938ca3977220a2674dc60a5
